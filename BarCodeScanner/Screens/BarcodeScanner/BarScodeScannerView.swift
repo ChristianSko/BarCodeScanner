3600e7346efd7822524071fb0b67e30aaf245c4d
Christian Skorobogatow <christiansko@icloud.com>
2021-02-05 00:01:53 +0100

    refactor for cleaner alert use

--- BarCodeScanner/Screens/BarcodeScanner/BarScodeScannerView.swift
+++ BarCodeScanner/Screens/BarcodeScanner/BarScodeScannerView.swift
@@ -17,7 +17,7 @@ struct BarScodeScannerView: View {
         NavigationView {
             VStack{
                 ScannerView(scannedCode: $viewModel.scannedCode, alertItem: $viewModel.alertItem)
-                    .frame(maxWidth: .infinity , maxHeight: 300)
+                    .frame(maxHeight: 300)
                 
                 Label("Scanner Barcode", systemImage: "barcode.viewfinder")
                     .font(.title)
@@ -36,8 +36,8 @@ struct BarScodeScannerView: View {
             }
             .navigationTitle("Barcode Scannner")
             .alert(item: $viewModel.alertItem) { alertItem in
-                Alert(title: Text(alertItem.title),
-                      message: Text(alertItem.message),
+                Alert(title: alertItem.title,
+                      message: alertItem.message,
                       dismissButton: alertItem.dismissButton)
             }
         }
